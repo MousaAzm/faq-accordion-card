3e7ab48e0a982cb6356879d2130e0aaf21d247b6
Mousa Azimi <mousa.azimi77@gmail.com>
2022-06-16 13:48:29 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+const accordionItems = document.querySelectorAll(".accordion > details");
+
+const siblings = (el) => {
+  if (el.parentNode === null) return [];
+  return Array.prototype.filter.call(el.parentNode.children, function (child) {
+    return child !== el;
+  });
+};
+
+accordionItems.forEach((el) => {
+  el.addEventListener("click", () => {
+    const others = siblings(el);
+    others.forEach((sibling) => {
+      sibling.removeAttribute("open");
+    });
+  });
+});
